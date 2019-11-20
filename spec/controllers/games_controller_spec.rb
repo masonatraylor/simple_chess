af732ecd16713d6d67b695fcb93b98291029b029
Mason <mason.a.traylor@gmail.com>
2019-11-20 13:51:56 -1000

    Add rspec

---
+++ spec/controllers/games_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe GamesController, :type => :controller do
+  context 'initial commit context' do
+    it 'will pass a test' do
+      expect(true).to be true
+    end
+  end
+end
