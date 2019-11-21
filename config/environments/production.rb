fc03b670b668d0cdce3768d2d5113e03a8e47601
Mason <mason.a.traylor@gmail.com>
2019-11-21 13:51:42 -1000

    Add font-awesome, simple_form and devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.default_url_options = { host: 'masons-simple-chess.herokuapp.com' }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'games#index'
   get 'games/index'
+  devise_for :users
 end
