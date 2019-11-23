eaddae07f80ed9750d1172069a4e5dd59e1676b8
Mason <mason.a.traylor@gmail.com>
2019-11-22 14:28:28 -1000

    Add obstruction_by method to piece

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -2,4 +2,41 @@
 
 class Piece < ApplicationRecord
   belongs_to :game
+
+  def obstructed_by(xpos, ypos)
+    return nil unless valid_coords?(xpos, ypos)
+
+    piece_blocking(xpos, ypos)
+  end
+
+  private
+
+  def valid_coords?(xpos, ypos)
+    x_diff = xpos - x_position
+    y_diff = ypos - y_position
+    x_diff.zero? || y_diff.zero? || x_diff.abs == y_diff.abs
+  end
+
+  def piece_blocking(xpos, ypos)
+    x_inc = inc(x_position, xpos)
+    y_inc = inc(y_position, ypos)
+    x = x_position + x_inc
+    y = y_position + y_inc
+    until x == xpos && y == ypos
+      piece = game.pieces.where(x_position: x, y_position: y).take
+      return piece if piece
+
+      x += x_inc
+      y += y_inc
+    end
+
+    nil
+  end
+
+  def inc(from, to)
+    return 1 if from < to
+    return -1 if from > to
+
+    0
+  end
 end
--- test/factories.rb
+++ test/factories.rb
@@ -1,9 +1,13 @@
 # This will guess the User class
 FactoryBot.define do
   factory :user do
-    email { "test@user.gov" }
-    password  { "aaaaaaa" }
+    email { 'test@user.gov' }
+    password { 'aaaaaaa' }
   end
+end
+
+FactoryBot.define do
   factory :game do
+    user { build(:user) }
   end
 end
