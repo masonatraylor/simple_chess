6097f73e91af072279b1278ab11035dbcf2af82f
Mason <mason.a.traylor@gmail.com>
2019-11-24 08:22:05 -1000

    Add valid bishop moves

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class Bishop < Piece
+  def valid_moves
+    moves = []
+    directions = [1, -1].product([1, -1])
+
+    (0..7).each do |i|
+      directions.each do |x_dir, y_dir|
+        moves << [x_position + x_dir * i, y_position + y_dir * i]
+      end
+    end
+
+    moves.reject { |x, y| invalid_move?(x, y) }
+  end
 end
