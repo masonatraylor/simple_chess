518671b92bd93cf39dc20191a7d36f860daf6f11
Mason <mason.a.traylor@gmail.com>
2020-01-02 16:06:38 -1000

    Automatically sign in users once confirmed and render games index

--- db/schema.rb
+++ db/schema.rb
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2020_01_03_013514) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "confirmation_token"
+    t.string "unconfirmed_email"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
